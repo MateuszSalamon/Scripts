5777f4868902499f7869a37553ecd6d432cbc04a
Mateusz <34424986+MateuszSalamon@users.noreply.github.com>
2019-03-25 19:19:45 +0100

    Added programming enhancer initial commit
    Added programming enhancer initial commit to new repo

---
+++ programming_enhancer.py
@@ -0,0 +1,25 @@
+import pyautogui
+import sys
+import random
+
+
+
+try:
+    set_range = 1000
+    for x in range(0, set_range):
+        y = random.random()
+        # pyautogui.press('numlock') #available also with numlock for only 3.99$
+        # print(y)
+        # pyautogui.PAUSE = y
+        pyautogui.press('capslock')
+        pyautogui.PAUSE = y
+        print(y*2)
+
+        #
+
+
+    print("done")
+except KeyboardInterrupt:
+    sys.exit(0)
+    raise
+
