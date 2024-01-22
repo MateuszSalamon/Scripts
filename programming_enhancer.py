11897593bb610d2dbefb4347fc34d3a7995afc17
Mateusz <34424986+MateuszSalamon@users.noreply.github.com>
2024-01-22 10:39:41 +0100

    Update programming_enhancer.py
    new version

--- programming_enhancer.py
+++ programming_enhancer.py
@@ -1,25 +1,33 @@
 import pyautogui
 import sys
 import random
-
-#use gloves for stronger effect 
-
-try:
-    set_range = 1000
-    for x in range(0, set_range):
-        y = random.random()
-        # pyautogui.press('numlock') #available also with numlock for only 3.99$
-        # print(y)
-        # pyautogui.PAUSE = y
-        pyautogui.press('capslock')
-        pyautogui.PAUSE = y
-        print(y*2)
-
-        
+import time as tm
 
 
-    print("done")
-except KeyboardInterrupt:
-    sys.exit(0)
-    raise
+
+# try:
+set_range = 400000
+full = 0
+pyautogui.FAILSAFE = False
+for x in range(0, set_range):
+    # y = random.random(0,10)
+    y = random.randint(10,30)
+    # pyautogui.press('numlock') #available also with numlock for only 3.99$
+    # print(y)
+    # pyautogui.PAUSE = y
+    pyautogui.press('scrolllock')
+    # time = y*10
+    time = y*2
+    pyautogui.PAUSE = time
+    full += time
+    #tm.strftime('%H:%M:%S', tm.gmtime(time))
+    print(x, time, full, tm.strftime('%H:%M:%S', tm.gmtime(full)))
+
+    #
+
+
+print("done")
+# except KeyboardInterrupt:
+#     sys.exit(0)
+#     raise
 
